889d1484062fd42c8d390163031a449e91252132
Sanket Jaiswal <103429178+SanketJ29@users.noreply.github.com>
2023-10-09 00:00:00 +0530

    Update Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -40,14 +40,15 @@ const Welcome = () => {
   return (
     <div>
       <section class="text-gray-400 bg-gray-900 body-font min-h-screen">
-        <div class="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
-            <img class="object-cover object-center rounded" alt="hero" src="https://www.linkpicture.com/q/49c669cb-55cc-4255-8a20-142c724a7012_1.jpeg"/>
+        <div class="container mx-auto flex px-10 py-16 md:flex-row flex-col items-center">
+          <div class="lg:max-w-xl lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
+{/*             <img class="object-cover object-center rounded" alt="hero" src="https://www.linkpicture.com/q/49c669cb-55cc-4255-8a20-142c724a7012_1.jpeg"/> */}
+            <img class="object-cover object-center rounded" alt="hero" src="https://www.linkpicture.com/q/heart-2658206_1280.webp"/>
           </div>
           <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 class="title-font text-5xl mb-12 font-medium text-cyan-400 font-mono">WELLNESS SAGA</h1>
-            <p class="mb-3 leading-9 text-1.5xl text-cyan-500 font-mono">Introducing "Wellness Saga," our award-winning Blockchain-based Healthcare platform. We emerged victorious in the Filecoin track at the "<a href="https://codecell-cmpn-vesit.ves.ac.in/Syrusnew.html" target="_blank" rel="noopener noreferrer " class="text-cyan-500 underline hover:text-cyan-600">SYRUS HACKATHON</a>" 2023, organized by <a href="https://vesit.ves.ac.in/code-cell" target="_blank" rel="noopener noreferrer " class="text-cyan-500 underline hover:text-cyan-600">CodeCell-VESIT</a>, and Devfolio. It transforms healthcare by securely storing medical data on an immutable blockchain called Filecoin, promoting interoperability, data privacy, and decentralization.</p>
-            <p class="mb-5 leading-relaxed text-1.5xl text-cyan-500 font-mono">Join us in shaping the future of healthcare, where health truly becomes wealth.</p>
+            <h1 class="title-font text-6xl mb-12 font-medium text-cyan-400 font-mono">WELLNESS SAGA</h1>
+            <p class="mb-3 leading-9 text-2xl text-cyan-500 font-mono">Introducing "Wellness Saga," our award-winning Blockchain-based Healthcare platform. We emerged victorious in the Filecoin track at the "<a href="https://codecell-cmpn-vesit.ves.ac.in/Syrusnew.html" target="_blank" rel="noopener noreferrer " class="text-cyan-500 underline hover:text-cyan-600">SYRUS HACKATHON</a>" 2023, organized by <a href="https://vesit.ves.ac.in/code-cell" target="_blank" rel="noopener noreferrer " class="text-cyan-500 underline hover:text-cyan-600">CodeCell-VESIT</a>, and Devfolio. It transforms healthcare by securely storing medical data on an immutable blockchain called Filecoin, promoting interoperability, data privacy, and decentralization.</p>
+            <p class="mb-5 leading-relaxed text-2xl text-cyan-500 font-mono">Join us in shaping the future of healthcare, where health truly becomes wealth.</p>
             <div class="flex justify-center">
               <button type="button" id="connectWalletButton"
                 className="flex flex-row justify-center items-center my-2 bg-cyan-700 p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
